a881639cf65869034ede3781b06de052de718a9d
Lucio Paiva <luciopaiva@gmail.com>
2021-06-15 23:26:44 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace HotKeyRepeat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.checkBoxTimerEnabled = new System.Windows.Forms.CheckBox();
+            this.textBoxInterval = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxHotKey = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxProcessName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Interval = 5000;
+            this.timer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBoxTimerEnabled
+            // 
+            this.checkBoxTimerEnabled.AutoSize = true;
+            this.checkBoxTimerEnabled.Location = new System.Drawing.Point(221, 82);
+            this.checkBoxTimerEnabled.Name = "checkBoxTimerEnabled";
+            this.checkBoxTimerEnabled.Size = new System.Drawing.Size(68, 19);
+            this.checkBoxTimerEnabled.TabIndex = 1;
+            this.checkBoxTimerEnabled.Text = "Enabled";
+            this.checkBoxTimerEnabled.UseVisualStyleBackColor = true;
+            this.checkBoxTimerEnabled.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // textBoxInterval
+            // 
+            this.textBoxInterval.Location = new System.Drawing.Point(107, 79);
+            this.textBoxInterval.Name = "textBoxInterval";
+            this.textBoxInterval.Size = new System.Drawing.Size(100, 23);
+            this.textBoxInterval.TabIndex = 2;
+            this.textBoxInterval.Text = "5000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Interval (ms):";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(14, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 116);
+            this.label2.TabIndex = 4;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Hot key:";
+            // 
+            // textBoxHotKey
+            // 
+            this.textBoxHotKey.Location = new System.Drawing.Point(107, 46);
+            this.textBoxHotKey.Name = "textBoxHotKey";
+            this.textBoxHotKey.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHotKey.TabIndex = 5;
+            this.textBoxHotKey.Text = "{F11}";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Process name:";
+            // 
+            // textBoxProcessName
+            // 
+            this.textBoxProcessName.Location = new System.Drawing.Point(107, 13);
+            this.textBoxProcessName.Name = "textBoxProcessName";
+            this.textBoxProcessName.Size = new System.Drawing.Size(182, 23);
+            this.textBoxProcessName.TabIndex = 7;
+            this.textBoxProcessName.Text = "Aseprite";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 252);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxProcessName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxHotKey);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxInterval);
+            this.Controls.Add(this.checkBoxTimerEnabled);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "HotKey Repeat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.CheckBox checkBoxTimerEnabled;
+        private System.Windows.Forms.TextBox textBoxInterval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxHotKey;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxProcessName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+namespace HotKeyRepeat
+{
+    public partial class Form1 : Form
+    {
+        private static bool DEBUG_CONSOLE = false;
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool AllocConsole();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private IntPtr GetAsepriteMainWindowHandle()
+        {
+            Process[] processList = Process.GetProcesses();
+
+            foreach (Process process in processList)
+            {
+                if (process.ProcessName == textBoxProcessName.Text)
+                {
+                    return FindWindow(null, process.MainWindowTitle);
+                }
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private void SendHotKeyToAseprite()
+        {
+            IntPtr aseprite = GetAsepriteMainWindowHandle();
+            IntPtr foreground = GetForegroundWindow();
+
+            // if Aseprite is not in the foreground, it's unlikely there were recent changes anyway
+            if (aseprite == foreground)
+            {
+                debug("Sending hotkey!");
+                SendKeys.Send(textBoxHotKey.Text);
+            } else
+            {
+                debug("Aseprite is not in the foreground");
+            }
+        }
+
+        private void debug(String msg)
+        {
+            if (DEBUG_CONSOLE)
+            {
+                Console.WriteLine(msg);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (DEBUG_CONSOLE)
+            {
+                AllocConsole();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SendHotKeyToAseprite();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxTimerEnabled.Checked)
+            {
+                try
+                {
+                    Int32 interval = Int32.Parse(textBoxInterval.Text);
+                    if (interval > 0)
+                    {
+                        timer.Interval = interval;
+                    } else
+                    {
+                        checkBoxTimerEnabled.Checked = false;
+                        return;
+                    }
+                } catch (FormatException ex)
+                {
+                    checkBoxTimerEnabled.Checked = false;
+                    return;
+                }
+            }
+
+            timer.Enabled = checkBoxTimerEnabled.Checked;
+            textBoxInterval.Enabled = !checkBoxTimerEnabled.Checked;
+            textBoxHotKey.Enabled = !checkBoxTimerEnabled.Checked;
+            textBoxProcessName.Enabled = !checkBoxTimerEnabled.Checked;
+        }
+    }
+}
